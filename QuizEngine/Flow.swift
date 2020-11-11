1503d9e609dc3cb116824f5c1a802ea8929aa950
zip520123 <zip520123@yahoo.com.tw>
2020-11-11 19:17:49 +0000

    add answer and result

--- QuizEngine/Flow.swift
+++ QuizEngine/Flow.swift
@@ -10,10 +10,12 @@ import Foundation
 protocol Router {
     typealias AnswerCallback = (String) -> Void
     func routeTo(question: String, answerCallback: @escaping AnswerCallback)
+    func routeTo(result: [String:String])
 }
 class Flow {
     private let router: Router
     private let questions: [String]
+    private var result: [String: String] = [:]
     init(questions:[String], router: Router){
         self.router = router
         self.questions = questions
@@ -22,20 +24,30 @@ class Flow {
     func start() {
         if let question = questions.first {
             router.routeTo(question: question, answerCallback: routeNext(from: question))
+        } else {
+            router.routeTo(result: [:])
         }
+        
     }
     
     private func routeNext(from question:String) -> Router.AnswerCallback {
-        return { [weak self] _ in
-            guard let self = self else {return}
-            if let currentQuestionIndex = self.questions.firstIndex(of: question) {
-                if currentQuestionIndex + 1 < self.questions.count {
-                    let nextQuestion = self.questions[currentQuestionIndex + 1]
-                    self.router.routeTo(question: nextQuestion, answerCallback: self.routeNext(from: nextQuestion))
-                    
-                }
-            }
+        return { [weak self] in
+            self?.routeNext(question, $0)
             
+        }
+    }
+    
+    private func routeNext(_ question: String,_ answer: String) {
+        if let currentQuestionIndex = questions.firstIndex(of: question) {
+            result[question] = answer
+            let nextIndex = currentQuestionIndex + 1
+            if nextIndex < questions.count {
+                let nextQuestion = questions[nextIndex]
+                router.routeTo(question: nextQuestion, answerCallback: routeNext(from: nextQuestion))
+                
+            } else {
+                router.routeTo(result: result)
+            }
             
         }
     }
